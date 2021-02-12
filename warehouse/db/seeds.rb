f78dd3b775d47bc8df4c440da8356f32993c64f7
Elliot <elliot.hayeon.cho@gmail.com>
2021-02-11 21:25:41 -0500

    cleanup and db seed

--- warehouse/db/seeds.rb
+++ warehouse/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+customer_a = Customer.create(name: 'Customer A', charge_type: :volume, charge_value: 3, flat_fee: 20)
+Customer.create(name: 'Customer B', charge_type: :volume, charge_value: 1, flat_fee: 20)
+Customer.create(name: 'Customer C', charge_type: :value, charge_value: 5, flat_fee: 20)
+customer_d = Customer.create(name: 'Customer D', charge_type: :volume, charge_value: 2, flat_fee: 20)
+
+Discount.create(customer: customer_a, percentage_off: 10, item_coverage_from: 0)
+Discount.create(customer: customer_d, percentage_off: 5, item_coverage_from: 0, item_coverage_to: 100)
+Discount.create(customer: customer_d, percentage_off: 10, item_coverage_from: 101, item_coverage_to: 200)
+Discount.create(customer: customer_d, percentage_off: 15, item_coverage_from: 201)
