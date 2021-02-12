7dbcda21aaf50d6ddf4864721b208795f6a27de8
Elliot <elliot.hayeon.cho@gmail.com>
2021-02-11 21:29:12 -0500

    fix

--- warehouse/spec/requests/customers_request_spec.rb
+++ warehouse/spec/requests/customers_request_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe 'Customers', type: :request do
-  describe 'GET /index' do
+  describe 'GET index' do
     it 'returns http success' do
-      get '/customers/index'
+      get '/customers'
       expect(response).to have_http_status(:success)
     end
   end
