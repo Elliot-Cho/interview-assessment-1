9ce6919e607134a271f71b2f3c06e0edc4bda67a
Elliot <elliot.hayeon.cho@gmail.com>
2021-02-11 19:11:48 -0500

    coalesce price quote interactions

--- warehouse/app/interactions/customers/quote_pricing_from_input.rb
+++ warehouse/app/interactions/customers/quote_pricing_from_input.rb
@@ -3,7 +3,7 @@ require 'active_interaction'
 module Customers
   # Interaction to quote customers on the pricing of storing their items
   class QuotePricingFromInput < ActiveInteraction::Base
-    object :customer
+    object :customer, class: Customer
 
     string :input
 
@@ -14,42 +14,10 @@ module Customers
       json_data = JSON.parse(input)['items']
       items = json_data.map { |item_params| Item.new(item_params) }
 
-      customer.flat_fee + calculate_pricing(items)
+      ::CustomerHelper::PriceQuoter.quote_pricing(customer, items)
     end
 
     private
-
-    def calculate_pricing(items)
-      send("pricing_by_#{customer.charge_type}", items)
-    end
-
-    def pricing_by_volume(items)
-      items.each_with_index.map { |item, index|
-        price = item.volume * customer.charge_value
-        discount = price * (discount_percentage_for_item_index(index + 1) / 100)
-
-        price - discount
-      }.sum
-    end
-
-    def pricing_by_value(items)
-      items.each_with_index.map { |item, index|
-        price = item.value * (customer.charge_value / 100)
-        discount = price * (discount_percentage_for_item_index(index + 1) / 100)
-
-        price - discount
-      }.sum
-    end
-
-    def discount_percentage_for_item_index(index)
-      discount = customer.discounts.find_by(
-        'item_coverage_from <= ? AND (item_coverage_to IS NULL OR item_coverage_to >= ?)',
-        index,
-        index
-      )
-
-      discount ? discount.percentage_off : 0
-    end
 
     # Validation
     def valid_json
